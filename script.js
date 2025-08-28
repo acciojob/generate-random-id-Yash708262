e3701cae58b95f03088442e86f324e0c34020aab
Yash708262 <yash708262@gmail.com>
2025-08-28 10:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
-  // write your code here
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for(let i = 0; i < n; i++){
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        result += chars[randomIndex]; // ✅ append karna hai
+    }
+    return result;
 }
 
 // Do not change the code below.
